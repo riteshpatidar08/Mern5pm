d50e7b4e64d77de846ea3d45234d9bb78103ddfd
Ritesh Patidar <itsmeriteshpatidar@gmail.com>
2024-11-09 18:38:34 +0530

    updated till day05

--- Day05/script.js
+++ Day05/script.js
@@ -12,10 +12,10 @@ const companies = ['meta','google','netflix'] ;
 //returns the length of new array
 const pushOutput = companies.push('tcs') ;
 
-console.log(pushOutput) ;
+// console.log(pushOutput) ;
 
 //check if original array changed ;
-console.log(companies)  ;
+// console.log(companies)  ;
 
 //NOTE - 
 const numbersArray = [1,2,3,4] ;
@@ -26,7 +26,7 @@ for(let item of numbersArray){
     newArr.push(item)
 }
 
-console.log(newArr)
+// console.log(newArr)
 
 for(let i=0 ; i < numbersArray.length ; i++){
     newArr.push(numbersArray[i]) ;
@@ -36,35 +36,125 @@ for(let i=0 ; i < numbersArray.length ; i++){
 const popOutput = companies.pop();
 
 //NOTE returns the element which is deleted from the array ;
-console.log(popOutput) ;
-console.log(companies) ;
+// console.log(popOutput) ;
+// console.log(companies) ;
 
 //NOTE unshift  ;
 
 const unshiftOutput = companies.unshift('grras') ;
 
 //return new array length
-console.log(unshiftOutput);
+// console.log(unshiftOutput);
 // do not mutate the original array 
-console.log(companies) ;
+// console.log(companies) ;
 
 //NOTE shift ;
 
 const shiftOutput = companies.shift() ;
 
-console.log(shiftOutput) ;
-console.log(companies) ;
+// console.log(shiftOutput) ;
+// console.log(companies) ;
 
 //NOTE includes (return true or false) ;
 
 const includesItem = companies.includes('tcs') ;
-console.log(includesItem) ;
+// console.log(includesItem) ;
 
 //NOTE ; 
 
-//you have to create a new array 
+//NOTE you have to create a new array 
 // then you have to add a new item to that array
-//if the item is already exist in the array console.log('item already exist) if item is not in the array push the item in the array ;
+//NOTE if the item is already exist in the array console.log('item already exist) if item is not in the array push the item in the array ;
+
+const products = ['tv','phone','ac'] ;
+
+const newProduct = 'ac';
+
+if(!products.includes(newProduct)){
+    products.push(newProduct);
+}else{
+    console.log('item already exists')
+}
+
+// console.log(products) ;
+
+
+//NOTE Slice  to extract the part of an array 
+
+const newProductsArr = ['tv','ac','fridge','mobile','heater','washingmachine'] ; 
+
+const slicedOutput = newProductsArr.slice(1,2) ;
+
+//return new array 
+// console.log(slicedOutput) ;
+
+// console.log(newProductsArr) ;
+
+//NOTE SPlice(add element to any particular index ,remove element) ;(
+
+const splicedOutput  = newProductsArr.splice(0,0,'tshirt','shirt') ;
+
+console.log(splicedOutput);
+
+//Note splice method changed the original array 
+console.log(newProductsArr) ; 
+
+//NOTE FindIndex() ;
+
+const numbersArr = [1,2,3,4,5] ; 
+
+// function itemCheck(item){
+//     return item < 5
+// }
+
+// callback function = ()=> {}
+
+//NOTE in array the methods which accepts a function is called higher order method , the function we pass in higher order method is called callback function.
+
+const findOutput = numbersArr.findIndex((item)=>{
+   return item > 5 ;
+})
+
+console.log(findOutput);   
+
+
+
+const newwProduct = ['tv','ac','fridge','mobile','heater','washingmachine'] ; 
+
+const indexOutput = newwProduct.findIndex((item)=>{
+return item === 'ac'
+})
+
+console.log(indexOutput)
+
+
+// for(let i = 0 ; i < newwProduct.length ; i++){
+//     if(newwProduct[i]==='ac'){
+//         console.log(i)
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
