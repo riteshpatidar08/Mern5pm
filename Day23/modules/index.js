7b29487b7a329d767a75ee0d1a7db47bd1da1a37
Ritesh Patidar <itsmeriteshpatidar@gmail.com>
2024-12-05 17:32:43 +0530

    updated till the node moduels

---
+++ Day23/modules/index.js
@@ -0,0 +1,14 @@
+
+
+
+export const college = 'iit' ;
+export const department = 'cs' ;
+
+
+// export default fetchData()
+//Named export => ki main ek file se multiple variables ya functions export krskta hu
+
+//default export => ki main kewal koi ek function ya variable export krskta hu
+
+// {college : collge , department : department}
+
---
+++ Day23/modules/script.js
@@ -0,0 +1,9 @@
+
+
+// import {college,department} from './index.js'
+// import * as col from './index.js' 
+// console.log(col.college , col.department)
+// console.log(college)
+//NOTE named export
+// console.log(fetchData)
+
